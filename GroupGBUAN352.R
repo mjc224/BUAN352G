5a6cd7ef04773b6fa825fa09f77172f83de2339f
mjc224 <89607895+mjc224@users.noreply.github.com>
2023-12-04 10:17:33 -0500

    Update GroupGBUAN352.R

--- GroupGBUAN352.R
+++ GroupGBUAN352.R
@@ -5,22 +5,54 @@
 #-----------
 #MSFT
 library(forecast)
-str(MSFT)
+MSFT <- read.csv("MSFT.csv")
+MSFT$date <- as.Date(MSFT$date, format="%m/%d/%Y")
+
+#subset to start at 2019
+MSFT.5yr <- subset(MSFT, date >= "2019-01-01")
 
 #252 trading days a year
-MSFT.ts <- ts(MSFT$close, frequency = 252) 
-plot(MSFT.ts)
+MSFT.5yr.ts <- ts(MSFT.5yr$close, frequency = 252) 
+plot(MSFT.5yr.ts, xaxt = "n", main = "MSFT Time Series")
+axis(1, at=1:6, labels=c(2019,2020,2021,2022,2023,2024))
+
+
+#subset to make it start from 2022
+MSFT.2yr <- subset(MSFT, date >= "2022-01-01")
+#252 trading days a year
+MSFT.2yr.ts <- ts(MSFT.2yr$close, frequency = 252)
+plot(MSFT.2yr.ts)
+plot(MSFT.2yr.ts, xaxt = "n", main = "MSFT Time Series")
+axis(1, at=1:2, labels=c(2023,2024))
+
+#subset to make 1 yr
+MSFT.1yr <- subset(MSFT, date >= "2022-11-30")
+#252 trading days a year
+MSFT.1yr.ts <- ts(MSFT.1yr$close, frequency = 252) 
+plot(MSFT.1yr.ts, xaxt = "n", main = "MSFT Time Series")
+axis(1, at=c(1,2), labels=c("11/30/2022","11/30/2023"))
+
+
+#subset to make 100 day
+MSFT.100day <- subset(MSFT, date >= "2023-7-12" & date < "2023-12-01")
+#252 trading days a year
+MSFT.100day.ts <- ts(MSFT.100day$close, frequency = 252)
+plot(MSFT.100day.ts)
+plot(MSFT.100day.ts, xaxt = "n", main = "MSFT Time Series")
+axis(1, at=c(1,1.2,1.4), labels=c("7/12/2023", "9/20/2023", "11/30/2023"))
 
 options(scipien=999)
-train.lm <- tslm(MSFT.ts ~ trend + season)
-summary(train.lm)
+MSFT.lm <- tslm(MSFT.ts ~ trend + season)
+summary(MSFT.lm)
 
 
 #----------------
 #AMZN
 library(forecast)
 AMZN <- read.csv("AMZN.csv")
-str(AMZN)
+AMZN$date <- as.Date(AMZN$date, format="%m/%d/%Y")
+AMZN <- subset(AMZN, date >= "2018-01-01")
+
 
 #252 trading days a year
 AMZN.ts <- ts(AMZN$close, frequency = 252) 
@@ -34,7 +66,8 @@ summary(AMZN.lm)
 #NVDA
 library(forecast)
 NVDA <- read.csv("NVDA.csv")
-str(NVDA)
+NVDA$date <- as.Date(NVDA$date, format="%m/%d/%Y")
+NVDA <- subset(NVDA, date >= "2018-01-01")
 
 #252 trading days a year
 NVDA.ts <- ts(NVDA$close, frequency = 252) 
